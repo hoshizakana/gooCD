136b8494dfc3baae63ac8fa103dcde0867cb524f
Saddy <saddy.yuji@gmail.com>
2019-08-11 10:24:54 +0000

    Orderモデルを削除＆作成

--- db/migrate/20190811102348_create_orders.rb
+++ db/migrate/20190811102348_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
       t.integer :user_id
-      t.string :shipping_status
+      t.string :shipping_status, default: "発送準備中"
       t.string :last_name
       t.string :first_name
       t.string :last_name_kana
