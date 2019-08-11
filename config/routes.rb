7f765e64e53aef92295b759db1683ef6a66e5b07
Saddy <saddy.yuji@gmail.com>
2019-08-11 12:16:31 +0000

    管理者のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,32 @@ Rails.application.routes.draw do
 
   get '/' => 'homes#top'
   get 'homes/ranking' => ''
-  get 'admin_products/new'
-  get 'admin_products/index'
-  get 'admin_products/show'
-  get 'admin_products/edit'
-  get 'admin_orders/index'
-  get 'admin_orders/show'
-  get 'admin_orders/edit'
-  get 'admin_users/show'
-  get 'admin_users/edit'
+
+#Saddy's
+  # admin_products
+  get '/admin/products/new' => 'admin_products#new'
+  get '/admin/products' => 'admin_products#index'
+  get '/admin/products/:id' => 'admin_products#show'
+  get '/admin/products/:id/edit' => 'admin_products#edit'
+  post '/admin/products' => 'admin_products#create'
+  patch '/admin/products/:id' => 'admin_products#update'
+# admin_orders
+  get '/admin/orders' => 'admin_orders#index'
+  get '/admin/orders/:id' => 'admin_orders#show'
+  get '/admin/orders/:id/edit' => 'admin_orders#edit'
+  patch '/admin/orders/:id' => 'admin_orders#update'
+# admin_users
+  get '/admin/users' => 'admin_users#index'
+  get '/admin/users/:id' => 'admin_users#show'
+  get '/admin/users/:id/edit' => 'admin_users#edit'
+  patch '/admin/useres/:id' => 'admin_users#update'
+#end
   get 'cart/index'
   get 'orders/process'
   get 'orders/confirm'
   get 'orders/complete'
-  get 'genres/index'
-  get 'labels/index'
+  get '/genres/index'
+  get '/labels/index'
   get 'artists/index'
   get 'products/index'
   get 'products/show'
@@ -39,6 +50,5 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
    }
 
-   get 'admin/users' => 'admin_users#index'
 
 end
