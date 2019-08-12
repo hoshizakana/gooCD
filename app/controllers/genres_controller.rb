b3a1e837f40dcf211c422341bba572b3afdd5e0d
KoutaroYamada <thringred@gmail.com>
2019-08-12 11:16:50 +0000

    ジャンル登録画面作成一時コミット

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,13 @@
 class GenresController < ApplicationController
   def index
+    @genre = Genre.new
+
+  end
+
+  def create
+    genre = Genre.new(params[:id])
+    genre.save
+    flash[:notice] = "保存されました。"
+    redirect_to ('/genres')
   end
 end
