91db9cdc21599a9866c1e3541c675ad3435f4b75
KoutaroYamada <thringred@gmail.com>
2019-08-14 00:26:56 +0000

    商品新規登録画面（仮）の作成　画像アップロードにエラーあり

--- app/controllers/admin_products_controller.rb
+++ app/controllers/admin_products_controller.rb
@@ -1,5 +1,20 @@
 class AdminProductsController < ApplicationController
   def new
+    @product = Product.new
+    8.times { @product.songs.build }
+    @artists = Artist.all  #リスト選択でなく、検索フォームからの検索に後で置き換え
+    @genres = Genre.all
+    @labels = Label.all   #リスト選択でなく、検索フォームからの検索に後で置き換え
+
+  end
+
+  def create
+    product = Product.new(product_params)
+    product.price.to_i
+    product.stock.to_i
+    product.save
+    flash[:notice] = "保存されました。"
+    redirect_to ('/admin/products/index')
   end
 
   def index
@@ -10,4 +25,22 @@ class AdminProductsController < ApplicationController
 
   def edit
   end
+
+  private
+  def product_params
+    params.require(:product).permit(
+      :name, 
+      :artist_id, 
+      :label_id, 
+      :genre_id, 
+      :status, 
+      :image_id, 
+      :price, 
+      :stock, 
+      :release_date, 
+      songs_attributes:[:id, :product_id, :name, :disk]
+    )
+
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  attachment :image
+  has_many :songs, dependent: :destroy
+  accepts_nested_attributes_for :songs
 end
