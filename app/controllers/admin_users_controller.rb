a8d4d022f59686b1462df145f5fadcfbb7961982
Saddy <saddy.yuji@gmail.com>
2019-08-15 15:05:38 +0000

    トップページの作成、他少々

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,10 +1,13 @@
 class AdminUsersController < ApplicationController
   def show
+		@user = User.find(parms[:id])
   end
 
   def edit
+		@user = User.find(parms[:id])
   end
 
   def index
+		@users = User.all
   end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,17 @@
 class HomesController < ApplicationController
   def top
+    @total_ranking = Product.limit(3)
+		@jpops3 = Product.where(genre_id: 1).limit(3)
+		@rocks3 = Product.where(genre_id: 2).limit(3)
+		@genres = Genre.all
+		# @fav_ranks = Product.find(Like.group(:product_id).order('count(product_id) desc').limit(3).pluck(:product_id))
   end
 
   def ranking
+			@ranking = Product.where(genre_id: 1).limit(50)
+			@genres = Genre.all
+		# @fav_ranks = Product.find(Like.group(:product_id).order('count(product_id) desc').limit(3).pluck(:product_id))
+		# @fav_ranks_genre = @fav_ranks.select{ |product| product.genre_id == id } # どうやってgenreIDを渡すんだっけ？
+		# あとでモデルのメソッドに移したい
   end
 end
