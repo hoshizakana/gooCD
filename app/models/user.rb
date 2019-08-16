25058df0d6225adf59cc51ccb5033ed763779aa1
Saddy <saddy.yuji@gmail.com>
2019-08-16 07:40:29 +0000

    一時コミット、developを再マージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,11 @@ class User < ApplicationRecord
 			.email(search_params[:email])
 			.phone(search_params[:phone])
 			.name(search_params[:name])
-		end
-		scope :id_like,    -> (id)    { where('id LIKE ?',    "%#{id}%") if id.present? }
-		scope :email_like, -> (email) { where('email LIKE ?', "%#{email}%") if email.present? }
-		scope :phone_like, -> (like)  { where('phone LIKE ?',  "%#{phone}%") if like.present? }
-		scope :name_like,  -> (name)  { where('name LIKE ?',  "%#{name}%") if name.present? }
+	end
+		# scope :id_like,    -> (id)    { where('id LIKE ?',    "%#{id}%") if id.present? }
+		# scope :email_like, -> (email) { where('email LIKE ?', "%#{email}%") if email.present? }
+		# scope :phone_like, -> (like)  { where('phone LIKE ?',  "%#{phone}%") if like.present? }
+		# scope :name_like,  -> (name)  { where('name LIKE ?',  "%#{name}%") if name.present? }
 	  scope :name_like,  -> (name)  { where('name LIKE ?',  "%#{name}%") if name.present? }
 
 end
