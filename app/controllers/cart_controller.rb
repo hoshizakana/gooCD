13743a59fc65e09856f9b8c1d2cdef61b289a743
Saddy <saddy.yuji@gmail.com>
2019-08-17 08:00:55 +0000

    カート機能作成中 一旦他の作業をマージ

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,16 +1,15 @@
 class CartController < ApplicationController
 
-# カート内商品の一覧
   def index
+		# カート内商品の一覧
 		@items = CartItem.where()
   end
-
 
 	def create
 		# 商品詳細画面で「カートに入れる」ボタンを押したときに動作する、新しいカート商品を保存する
 		cart_item = CartItem.new(cart_item_params)
-		cart_item = cart_item.item_number.to_i # 一応入れておきます
-		cart_item = save
+		cart_item.item_number.to_i # 一応入れておきます
+		cart_item.save
 		flash[:notice] = "カートに入れました。"
 		redirect_to ("/user/products/#{@product.id}") # 商品一覧から正しくproductが渡されるか？
 	end
@@ -24,7 +23,10 @@ class CartController < ApplicationController
 	def destroy
 		# カート画面で、削除を送った時に動作する
 		cart_item = CartItem.find(params[:id])
-
+		cart_item.destroy
+		flash[:notice] = "カートから出しました。"
+		redirect_to ("/carts/#{current_user.id}")
+	end
 
 	private
 	def cart_item
