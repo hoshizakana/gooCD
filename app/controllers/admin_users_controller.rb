1a852636aaa603f40f957dee23857ba1d65f71cb
Saddy <saddy.yuji@gmail.com>
2019-08-17 03:15:34 +0000

    管理者ユーザ一覧,詳細,編集,検索の動作確認完了、残り:購入履歴表示

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -7,6 +7,15 @@ class AdminUsersController < ApplicationController
   def edit
 		@user = User.find(params[:id])
   end
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = "会員情報の編集が完了しました"
+			redirect_to ("/admin/users/#{@user.id}")
+		else
+			render :edit
+		end
+	end
 
   def index
 		@search = User.ransack(params[:q])
@@ -14,4 +23,9 @@ class AdminUsersController < ApplicationController
 		# @users = User.all これなしでも、全件表示可能
 		# resultには空条件で検索した結果（つまりすべてのレコード）が入る為
   end
+
+	private
+	def user_params
+		params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :phone, :postal_code, :adress)
+	end
 end
