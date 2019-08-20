9e950c7c0dbdf1351bcc7fa7634e0b92a44e6328
DKDKDKDK0823 <daiki.peace@gmail.com>
2019-08-20 09:24:21 +0000

    細かい修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,35 @@
 //= require bootstrap-sprockets
 //= require nested_form_fields
 //= require_tree .
+
+
+
+        
+// $(document).ready(function() {
+//     $(function(){
+//         $('.product-index').mouseover(function(){
+//             $(this).addClass('changeColor');
+//             var img = $(this).children('img');
+//             console.log(img.attr('src'));
+//             $('body').fadeIn(3000);
+//                 $('body').css('background-image','url('+img.attr('src')+')');
+//                 $('body').css('background-repeat','no-repeat');
+//                 $('body').css('background-size' ,'100%');
+//                 $('body').css('background-position', 'center');
+                
+//                 $('.opacity').addClass('opacity_add')
+//                 // $('.opacity').css('height' ,'100%');
+//                 // $('.opacity').css('background' ,'rgba(255,255,255,0.4)');
+                
+            
+
+
+//         });
+//         $('.product-index').mouseout(function(){
+//             $(this).removeClass('changeColor');
+//             $('body').css('background-image','none');
+//             $('.opacity').removeClass('opacity_add')
+//         });
+//      });
+
+// });
