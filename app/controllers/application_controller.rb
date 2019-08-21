b58b29bc9050fd7f66aacf6731708f4c625d0204
KoutaroYamada <thringred@gmail.com>
2019-08-21 04:42:22 +0000

    管理者のパス・email登録のseed設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ before_action :set_search
     @searched_products = @q.result(distinct: true)
   end
 
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_orders_path
+    when User
+      "/"
+    end
+  end
+
 end
