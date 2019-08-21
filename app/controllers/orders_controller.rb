b728ff57c0a536c78cc5ad8081a6126ffd0b5add
Saddy <saddy.yuji@gmail.com>
2019-08-21 02:11:11 +0000

    とりあえず

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   def procedure
+		@order = Order.new
 		@cart_items = CartItem.where(user_id: current_user.id)
 		@addresses = Address.where(user_id: current_user.id)
 		@address_new = Address.new
@@ -7,30 +8,39 @@ class OrdersController < ApplicationController
 	def create_addresses
 		address = Address.new(address_params)
 		address.save
-		render process
+		render procedure
 	end
 
   def confirm
 		@order = Order.new
+		if @order.invalid?
+			render :procedure
+		end
   end
 	def create
 		order = Order.new(order_params)
-		order.save
-		cart_items = CartItem.where(user_id: current_user.id)
-		cart_items.each do |cart_item|
-			order_item = OrderItem.new
-			order_item.product_id = cart_item.product_id
-			order_item.item_number = cart_item.item_number
-			order_item.user_id = current_user.id
-			order_item.order_id = order.id
-			order_item.save
-			cart_item.delete #なんか違うかこれ？
+		if params[:back] #戻るボタンで送信された場合は
+			format.html{render procedure} #描画をリセットしprocedureを呼ぶ
+		elsif order.save
+			cart_items = CartItem.where(user_id: current_user.id)
+			cart_items.each do |cart_item|
+				#適合するcart_itemを取り出し、一つ一つをorder_itemに登録する
+				order_item = OrderItem.new
+				order_item.product_id = cart_item.product_id
+				order_item.item_number = cart_item.item_number
+				order_item.user_id = current_user.id
+				order_item.order_id = order.id
+				order_item.save
+				cart_item.delete #移したカートアイテムを削除する
+			end
+			redirect_to ("/order/complete") #購入完了ページへ
+		else
+			format.html{render procedure} #orderがセーブできなかったら戻る
+
 		end
-		redirect_to ("/order/complete")
 	end
 
   def complete
-
   end
 
 
@@ -66,7 +76,7 @@ class OrdersController < ApplicationController
 			:first_name_kana, # 同様
 			:postal_code, # 同様
 			:address, # 同様
-			:phone, # 同様
+			:phone # 同様
 		)
 	end
 end
