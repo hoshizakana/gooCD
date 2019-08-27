7ed3525ff1e724ab32966f6f9c5fd6c73ae87e93
NaofumiOkamoto <n.okamooo@gmail.com>
2019-08-27 16:00:02 +0900

    販売停止中

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -9,23 +9,29 @@ class CartController < ApplicationController
 	def create
 		#既に登録済みのカートアイテムを検索し、cart_itemに代入
 		cart_item = CartItem.where(user_id: current_user.id).where(product_id: cart_item_params[:product_id])
-
-		# もし登録済みのカートアイテムがあったら、登録済みのカートアイテムの数量に1を足す
-		if cart_item.present?
-			cart_item[0].item_number += 1
-			cart_item[0].save
-			flash[:success] = "「#{cart_item[0].product.name}」をカートに追加しました。"
-			redirect_to ("/products/#{cart_item[0].product_id}") # 商品一覧から正しくproductが渡されるか？
-			else
-		# 登録済みのカートアイテムがない場合、新たにカートアイテムを作成	
 		cart_new_item = CartItem.new(cart_item_params)
-		cart_new_item.user_id = current_user.id
-		cart_new_item.item_number = 1
-		cart_new_item.save
-    flash[:success] = "「#{cart_new_item.product.name}」をカートに追加しました。"
-		redirect_to ("/products/#{cart_new_item.product_id}") # 商品一覧から正しくproductが渡されるか？
-		end
-	end	
+    product = Product.find(cart_new_item.product_id)
+    # もし商品が販売中だったらカート追加の処理に入る
+    if product.status == "販売中"
+      # もし登録済みのカートアイテムがあったら、登録済みのカートアイテムの数量に1を足す
+      if cart_item.present?
+        cart_item[0].item_number += 1
+        cart_item[0].save
+        flash[:success] = "「#{cart_item[0].product.name}」をカートに追加しました。"
+        redirect_to ("/products/#{cart_item[0].product_id}") # 商品一覧から正しくproductが渡されるか？
+      else
+      # 登録済みのカートアイテムがない場合、新たにカートアイテムを作成	
+        cart_new_item.user_id = current_user.id
+        cart_new_item.item_number = 1
+        cart_new_item.save
+        flash[:success] = "「#{cart_new_item.product.name}」をカートに追加しました。"
+        redirect_to ("/products/#{cart_new_item.product_id}") # 商品一覧から正しくproductが渡されるか？
+      end
+    else
+      flash[:danger] = "この商品は販売停止中のため、カートに入れることができません"
+      redirect_to ("/products/#{product.id}")
+    end
+	end
 
 	def update
 		# カート画面で、数量変更を送った時に動作する
