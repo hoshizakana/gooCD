327f0c698ec88bd240f709d6b892f62d4d87b185
Saddy <saddy.yuji@gmail.com>
2019-08-28 07:56:25 +0000

    購入完了後戻るを押して再度確定するとバグが復活していたので再修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,28 @@ class Order < ApplicationRecord
 	has_many :order_items, dependent: :destroy
 	accepts_nested_attributes_for :order_items, allow_destroy: true
 	# a_n_a_fで「reject_if: all_blank」とすると、order_itemなしでorderが保存できるようになる
+
+	def set_order(index)
+		if index.to_i == 0
+			@order.last_name = current_user.last_name
+			@order.first_name = current_user.first_name
+			@order.last_name_kana = current_user.last_name_kana
+			@order.first_name_kana = current_user.first_name_kana
+			@order.postal_code = current_user.postal_code
+			@order.address = current_user.adress
+			@order.phone = current_user.phone
+		else
+			adr.Address.find(index.to_i)
+			@order.last_name = adr.last_name
+			@order.first_name = adr.first_name
+			@order.last_name_kana = adr.last_name_kana
+			@order.first_name_kana = adr.first_name_kana
+			@order.postal_code = adr.postal_code
+			@order.address = adr.address
+			@order.phone = adr.phone
+		end
+
+
+
+
 end
